d9664a45812c454fc773e46ecc33572181abb179
Jinpeng He <jinpeng.he@shopee.com>
2020-02-01 18:01:29 +0800

    add feature del book

--- lib/urls.py
+++ lib/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('detail/', views.detail, name='detail'),
     path('addBook/', views.addBook, name='addBook'),
+    path('delBook/<int:book_id>', views.deleteBook, name='delBook')
 ]
